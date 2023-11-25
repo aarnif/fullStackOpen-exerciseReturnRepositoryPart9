2c92dd7e76d66d732d6268ec49057c9127df36a7
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-11-25 15:11:26 +0200

    Exercise 9.4

---
+++ projectOne/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
