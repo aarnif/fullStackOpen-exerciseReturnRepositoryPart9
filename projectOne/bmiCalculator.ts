f7ca17e0e0ef9dd1caa7687e9630b24b42929851
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-11-25 15:36:27 +0200

    Exercise 9.5

--- projectOne/bmiCalculator.ts
+++ projectOne/bmiCalculator.ts
@@ -3,7 +3,7 @@ interface Values {
   b: number;
 }
 
-const parseArgumentsCalculateBMI = (args: string[]): Values => {
+export const parseArgumentsCalculateBMI = (args: string[]): Values => {
   if (args.length < 4) throw new Error("Not enough arguments");
   if (args.length > 4) {
     console.log("You provided more than two arguments");
@@ -19,37 +19,36 @@ const parseArgumentsCalculateBMI = (args: string[]): Values => {
   }
 };
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const result = weight / (height / 100) ** 2;
-  const resultMessage = "Result: ";
 
   switch (true) {
     case result <= 16.0:
-      return resultMessage + "Underweight (Severe thinness)";
+      return "Underweight (Severe thinness)";
     case result >= 16.0 && result <= 16.9:
-      return resultMessage + "Underweight (Moderate thinness)";
+      return "Underweight (Moderate thinness)";
     case result >= 17.0 && result <= 18.4:
-      return resultMessage + "Underweight (Mild thinness)";
+      return "Underweight (Mild thinness)";
     case result >= 18.5 && result <= 24.9:
-      return resultMessage + "Normal (healthy weight)";
+      return "Normal (healthy weight)";
     case result >= 25.0 && result <= 29.9:
-      return resultMessage + "Overweight (Pre-obese)";
+      return "Overweight (Pre-obese)";
     case result >= 30.0 && result <= 34.9:
-      return resultMessage + "Obese (Class I)";
+      return "Obese (Class I)";
     case result >= 35.0 && result <= 39.9:
-      return resultMessage + "Obese (Class II)";
+      return "Obese (Class II)";
     case result >= 40.0:
-      return resultMessage + "Obese (Class III)";
+      return "Obese (Class III)";
     default:
-      return resultMessage + "no result";
+      return "no result";
   }
 };
 
-try {
-  const { a, b } = parseArgumentsToCalculateBMI(process.argv);
-  console.log(calculateBmi(a, b));
-} catch (error: unknown) {
-  if (error instanceof Error) {
-    throw new Error(error.message);
-  }
-}
+// try {
+//   const { a, b } = parseArgumentsCalculateBMI(process.argv);
+//   console.log(calculateBmi(a, b));
+// } catch (error: unknown) {
+//   if (error instanceof Error) {
+//     throw new Error(error.message);
+//   }
+// }
--- projectOne/index.ts
+++ projectOne/index.ts
@@ -1,10 +1,29 @@
 import express from "express";
-
+import qs from "qs";
+import { calculateBmi } from "./bmiCalculator";
 const app = express();
-const PORT = 3000;
+const PORT = 3003;
+
+app.use("query parser", (str: any) => qs.parse(str));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  let { height, weight } = req.query;
+
+  if (isNaN(Number(height)) || isNaN(Number(weight))) {
+    res.json({ error: "malformatted parameters" });
+  }
+
+  res.json({
+    weight: Number(height),
+    height: Number(weight),
+    bmi: calculateBmi(Number(height), Number(weight)),
+  });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
