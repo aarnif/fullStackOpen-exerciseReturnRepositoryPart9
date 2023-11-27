12940b5f7d0d33bcfa8c0e50fec4f3b19990f25f
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-11-27 11:15:07 +0200

    Exercise 9.9

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   res.send("pong");
 });
 
