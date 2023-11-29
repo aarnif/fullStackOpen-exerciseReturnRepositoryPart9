fcf0bb24bbd8a5a38cc7d842ff1ef7278477b33b
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-11-29 16:35:49 +0200

    Exercise 9.22

--- patientor-backend/types.ts
+++ patientor-backend/types.ts
@@ -10,7 +10,41 @@ export enum Gender {
   Other = "other",
 }
 
-export interface Entry {}
+export interface BaseEntry {
+  id: string;
+  date: string;
+  specialist: string;
+  description: string;
+  diagnosisCodes?: string[];
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating?: HealthCheckRating;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: object;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge?: object;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
